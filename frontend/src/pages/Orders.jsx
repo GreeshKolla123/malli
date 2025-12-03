3330a92bf2d275bdbb932d3283282ffdc3d02c2a
GreeshKolla123 <2100030262@kluniversity.in>
2025-12-03 11:08:36 +0530

    🚀 Initial commit - Generated with CODE AGENT

---
+++ frontend/src/pages/Orders.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Orders.css';
+
+function Orders() {
+  return (
+    <div className="orders">
+      <h1>Orders</h1>
+      <ul>
+        <li>Order 1</li>
+        <li>Order 2</li>
+        <li>Order 3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Orders;
